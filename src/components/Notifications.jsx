db18519dbcbce53f1edcee05a623975976faee8a
Sumit Paul <pauls1317@gmail.com>
2023-09-27 00:19:46 +0530

    caller name check

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,23 +1,23 @@
-import React, { useContext } from 'react'
-import { Button } from '@material-ui/core'
+import React, { useContext } from "react";
+import { Button } from "@material-ui/core";
 
-import { SocketContext } from '../SocketContext'
+import { SocketContext } from "../SocketContext";
 
 const Notifications = () => {
-    const {answerCall, call, callAccepted} = useContext(SocketContext);
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
 
   return (
     <>
-        {call.isReceivedCall && !callAccepted && (
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-                <h1>{call.name} is calling: </h1>
-                <Button variant='contained' color="primary" onClick={answerCall}>
-                    Answer
-                </Button>
-            </div>
-        )}
+      {call.isReceivedCall && !callAccepted && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <h1>{call.name || "Unknown"} is calling: </h1>
+          <Button variant="contained" color="primary" onClick={answerCall}>
+            Answer
+          </Button>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Notifications
+export default Notifications;
